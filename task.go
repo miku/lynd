78fc0e30de7fee45c34903059d37a93ee1672c57
Martin Czygan <martin.czygan@gmail.com>
2015-07-04 16:00:53 +0200

    attempt at defaults

--- task.go
+++ task.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"time"
 
 	"github.com/miku/structs"
 )
 
 type TagEvaluator func(Task) Task
+
+type FuncMap map[string]func(string) (string, error)
+
+var defaultsFuncs = FuncMap{
+	"today": func(value string) (string, error) {
+		return time.Now().Format("2006-01-02"), nil
+	},
+	"yesterday": func(value string) (string, error) {
+		return time.Now().Add(-24 * time.Hour).Format("2006-01-02"), nil
+	},
+}
 
 // setDefaults evaluates the default struct tag.
 func setDefaults(task Task) error {
@@ -20,6 +32,15 @@ func setDefaults(task Task) error {
 		v := field.Tag("default")
 		if v == "" {
 			continue
+		}
+		for key, f := range defaultsFuncs {
+			if v == key {
+				var err error
+				v, err = f(v)
+				if err != nil {
+					return err
+				}
+			}
 		}
 		switch field.Kind() {
 		case reflect.String:
--- task_test.go
+++ task_test.go
@@ -1,6 +1,9 @@
 package lynd
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 type noopTask struct{}
 
@@ -21,6 +24,16 @@ type testTaskInt struct {
 type testTaskFloat struct {
 	noopTask
 	Val float64 `default:"0.3"`
+}
+
+type testTaskToday struct {
+	noopTask
+	Date string `default:"today"`
+}
+
+type testTaskYesterday struct {
+	noopTask
+	Date string `default:"yesterday"`
 }
 
 func TestSetDefaultsString(t *testing.T) {
@@ -70,6 +83,40 @@ func TestSetDefaultsFloat(t *testing.T) {
 		setDefaults(&c.task)
 		if c.task.Val != c.Val {
 			t.Errorf("got %s, want %s", c.task.Val, c.Val)
+		}
+	}
+}
+
+func TestSetDefaultsToday(t *testing.T) {
+	var cases = []struct {
+		task testTaskToday
+		Date string
+	}{
+		{testTaskToday{}, time.Now().Format("2006-01-02")},
+		{testTaskToday{Date: "2012-02-02"}, "2012-02-02"},
+	}
+
+	for _, c := range cases {
+		setDefaults(&c.task)
+		if c.task.Date != c.Date {
+			t.Errorf("got %s, want %s", c.task.Date, c.Date)
+		}
+	}
+}
+
+func TestSetDefaultsYesterday(t *testing.T) {
+	var cases = []struct {
+		task testTaskYesterday
+		Date string
+	}{
+		{testTaskYesterday{}, time.Now().Add(-24 * time.Hour).Format("2006-01-02")},
+		{testTaskYesterday{Date: "2012-02-02"}, "2012-02-02"},
+	}
+
+	for _, c := range cases {
+		setDefaults(&c.task)
+		if c.task.Date != c.Date {
+			t.Errorf("got %s, want %s", c.task.Date, c.Date)
 		}
 	}
 }
